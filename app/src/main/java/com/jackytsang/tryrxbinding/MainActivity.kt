b5e57037b09b9150ddaa345e1e730078abefb72b
Jacky Tsang <szechunt@gmail.com>
2019-10-10 16:22:34 +0800

    use rxbinding to listen text changes

--- app/src/main/java/com/jackytsang/tryrxbinding/MainActivity.kt
+++ app/src/main/java/com/jackytsang/tryrxbinding/MainActivity.kt
@@ -2,11 +2,17 @@ package com.jackytsang.tryrxbinding
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jakewharton.rxbinding3.widget.textChanges
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edit_txt.textChanges().subscribe { text ->
+            echo_txt.text = text
+        }
     }
 }
